99176f5778c76ec14e84c696ec9945cafb3165a2
Robin Wyss <rowyss@gmail.com>
2012-03-14 22:04:03 +0100

    added js utils, put everything together

--- util.js
+++ util.js
@@ -1,16 +1,27 @@
-var Util = function () {
+var Util = (function () {
     return {
-        generateDivs:function (type, className) {
+        generateDivs:function (parent, count, className) {
             var element
-            for (var i = 0; i < 1000; i++) {
+            for (var i = 0; i < count; i++) {
                 element = document.createElement('div');
-                element.setAttribute('class', 'card');
-                R.byId('container').appendChild(element);
+                if (className) {
+                    element.setAttribute('class', className);
+                }
+                parent.appendChild(element);
+            }
+        },
+        byId:function (id) {
+            return document.getElementById(id);
+        },
+        onLoad:function (callback) {
+            Util.addEvent(window, 'load', callback);
+        },
+        addEvent:function (element, event, callback) {
+            if (element.addEventListener) {
+                element.addEventListener(event, callback, false);
+            } else if (element.attachEvent) {
+                window.attachEvent('on' + event, callback);
             }
         }
     }
-}
-
-function generateDivs() {
-
-}
+})();
